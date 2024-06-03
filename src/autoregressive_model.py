864fd6eaa70085516a149928690e41a8d26b2967
Najoung Kim <najoungk@gmail.com>
2024-06-03 17:01:52 -0400

    Syntax error fix

--- src/autoregressive_model.py
+++ src/autoregressive_model.py
@@ -52,6 +52,7 @@ def main():
             bnb_4bit_quant_type='nf4',
             bnb_4bit_use_double_quant=True,
             bnb_4bit_compute_dtype=torch.bfloat16
+        )
     if 'goat' in args.model_name_or_checkpoint:
         model = GoatModel(
             args.model_name_or_checkpoint,
